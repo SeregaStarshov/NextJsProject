935ceee8b58514549a7e84356dd5ac066d645613
serega.starshov <serega.starshov@mail.ru>
2022-01-03 12:57:29 +0300

    change

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,62 +1,63 @@
-import { FC } from 'react'
+import { FC } from 'react';
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import Image from 'next/image'
+import Image from 'next/image';
 import { Menu } from '../../types';
 import { styled } from '@material-ui/core/styles';
 import logo from '../../public/logo.png';
 import Navigation from './Menu';
-
+import { Grid } from '@mui/material';
 
 const CustomAppBar = styled(AppBar)({
-    backgroundColor: 'inherit',
-    boxShadow: 'none',
-    display: 'flex',
-    justifyContent: 'center',
+	backgroundColor: 'inherit',
+	height: '60px',
+	padding: '10px',
 });
 
 const Header: FC = () => {
-    const navMenu: Menu[] = [
-        {
-            id: '1',
-            link: 'ГЛАВНАЯ'
-        },
-        {
-            id: '2',
-            link: 'РЕЙТИНГИ'
-        },
-        {
-            id: '3',
-            link: 'БЛОГ'
-        },
-        {
-            id: '4',
-            link: 'PRO'
-        },
-        {
-            id: '5',
-            link: 'ИНВЕСТОРУ'
-        }
-    ]
-    return (
-        <CustomAppBar position="static">
-            <Toolbar sx={{
-                maxWidth: '1278px',
-                minHeight: '56px',
-                justifyContent: 'space-between',
-            }}>
-                <Image src={logo} alt='logo'/>
-                <Navigation menu={navMenu} />
-                <Button color="inherit">RU</Button>
-                <Button color="inherit">Войти</Button>
-                <Button color="inherit">Начать</Button>
-            </Toolbar>
-        </CustomAppBar>
-    );
-}
+	const navMenu: Menu[] = [
+		{
+			id: '1',
+			link: 'ГЛАВНАЯ',
+		},
+		{
+			id: '2',
+			link: 'РЕЙТИНГИ',
+		},
+		{
+			id: '3',
+			link: 'БЛОГ',
+		},
+		{
+			id: '4',
+			link: 'PRO',
+		},
+		{
+			id: '5',
+			link: 'ИНВЕСТОРУ',
+		},
+	];
+	return (
+		<CustomAppBar position='static'>
+			<Toolbar
+				variant='regular'
+				sx={{
+					maxWidth: '1278px',
+					justifyContent: 'space-between',
+				}}
+			>
+				<Image src={logo} alt='logo' />
+				<Navigation menu={navMenu} />
+				<Button color='inherit'>RU</Button>
+				<Button color='inherit'>Войти</Button>
+				<Button color='inherit'>Начать</Button>
+			</Toolbar>
+		</CustomAppBar>
+	);
+};
 
 export default Header;
--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -1,35 +1,41 @@
-import { FC } from "react";
-import Box from "@mui/material/Box";
+import React, { FC } from 'react';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-// import {Link} from "@mui/material";
-import {Menu} from '../../types';
-import Link from "next/link";
+import { Menu } from '../../types';
+import Link from 'next/link';
+import Grid from '@mui/material/Grid';
 
-
-const Navigation: FC<{menu: Menu[]}> = ({menu}) => {
-    return (
-        <Box sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            backgroundColor: 'inherit',
-            width: ''
-        }}>
-            {menu.map((item) => (
-                <Link href={`/${item.link}`} key={item.id} passHref>
-                    <Typography variant="h6" component="a" sx={{
-                        fontStyle: 'normal',
-                        fontWeight: 'normal',
-                        fontSize: '14px',
-                        lineHeight: '13px',
-                        color: '#FFFFFF',
-                    }}>
-                    {item.link}
-                    </Typography>
-                </Link>
-            ))}
-        </Box>
-    );
-}
+const Navigation: FC<{ menu: Menu[] }> = ({ menu }) => {
+	return (
+		<Grid>
+			{menu.map((item) => (
+				<Link href={`/${item.link}`} key={item.id} passHref>
+					<Typography
+						variant='h6'
+						component='a'
+						sx={{
+							fontStyle: 'normal',
+							fontWeight: 'normal',
+							fontSize: '14px',
+							lineHeight: '13px',
+							color: '#FFFFFF',
+							// '&:not(:last-child)': {
+							//     marginRight: '36px',
+							// }
+						}}
+					>
+						{item.link}
+					</Typography>
+				</Link>
+			))}
+		</Grid>
+	);
+};
 
 export default Navigation;
+
+// display: 'flex',
+//         //     justifyContent: 'space-between',
+//         //     alignItems: 'center',
+//         //     backgroundColor: 'inherit',
+//         //     marginRight: '60px'
